34703c4920fa476777c097a692944af5e108869a
tombetthauser <tombetthauser@gmail.com>
2020-05-06 23:12:09 -0700

    Get user input working to create and open a new folium map location

--- test.py
+++ test.py
@@ -1,17 +1,22 @@
 import folium
 import geopy
 import geopandas
-# print(dir(folium))
-map = folium.Map(location=[80,-100])
-map.save("Map.html")
-map2 = folium.Map(location=[38.58,-99.09], zoom_start=6)
-map2.save("Map2.html")
-map3 = folium.Map(location=[38.58,-99.09], zoom_start=10, tiles="Stamen Terrain")
-map3.add_child(folium.Marker(location=[38.58,-99.09], popup="Tom Betthauser's Studio", icon=folium.Icon(color='green')))
-map3.save("Map3.html")
+import os
+
+name = input("Please enter your name: ")
+city = input("Please enter your studio location (address, city, country): ")
 
 locator = geopy.geocoders.Nominatim(user_agent='myGeocoder')
-# locator = Nominatim(user_agent='myGeocoder')
-location = locator.geocode("Champ de Mars, Paris, France")
+location = locator.geocode(city)
 
-print('Latitude={}, Longitude={}'.format(location.latitude, location.longitude))
+# map = folium.Map(location=[80,-100])
+# map.save("Map.html")
+# map2 = folium.Map(location=[38.58,-99.09], zoom_start=6)
+# map2.save("Map2.html")
+
+map3 = folium.Map(location=[location.latitude,location.longitude], zoom_start=10, tiles="Stamen Terrain")
+map3.add_child(folium.Marker(location=[location.latitude,location.longitude], popup=f"{name}'s' Studio", icon=folium.Icon(color='green')))
+map3.save("Map3.html")
+
+# print('Latitude={}, Longitude={}'.format(location.latitude, location.longitude))
+os.system('open Map3.html')
