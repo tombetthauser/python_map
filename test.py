61974b543b1aae4508449fe4b39a47a50a5e6074
tombetthauser <tombetthauser@gmail.com>
2020-05-06 19:51:44 -0700

    Add geopy and geopandas imports for dynamic city geolocation lookups

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import folium
-print(dir(folium))
+import geopy
+import geopandas
+# print(dir(folium))
 map = folium.Map(location=[80,-100])
 map.save("Map.html")
 map2 = folium.Map(location=[38.58,-99.09], zoom_start=6)
 map2.save("Map2.html")
 map3 = folium.Map(location=[38.58,-99.09], zoom_start=10, tiles="Stamen Terrain")
+map3.add_child(folium.Marker(location=[38.58,-99.09], popup="Tom Betthauser's Studio", icon=folium.Icon(color='green')))
 map3.save("Map3.html")
+
+locator = geopy.geocoders.Nominatim(user_agent='myGeocoder')
+# locator = Nominatim(user_agent='myGeocoder')
+location = locator.geocode("Champ de Mars, Paris, France")
+
+print('Latitude={}, Longitude={}'.format(location.latitude, location.longitude))
